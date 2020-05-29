e9b8a72a7039bc46d709542a4a328a6bd62f0782
Brandon Ho Soon Zhi <szh19@ic.ac.uk>
2020-05-29 15:38:19 +0100

    Minor changes to structure of branch

--- src/instructions/branch.c
+++ src/instructions/branch.c
@@ -8,7 +8,10 @@
 //void branch(decoded_inst* inst, registers* rs) {
 void branch(machine_state *ms) {
   int32_t offset = ms->instrToExecute.branch_offset;
-  offset = shift_2_sign_extension(offset);
+  offset <<= 2;
+  if ((offset & (1 << 25)) != 0) { //check if bit 25 is 1
+    offset = ((63 << 26) | offset); //63 = pow(2, 7) - 1
+  }
   ms->regs.PC += offset;
   ms->ps = EMPTY;
 }
