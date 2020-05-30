c8f35f3ee3a1d35d54e02d9b73e169c0727d85c7
Ivy Tam <ivytam@Ivys-MacBook-Pro.local>
2020-05-30 15:13:33 +0800

    Changed decode using hard-codedvalues instead of extractBits

--- src/decode.c
+++ src/decode.c
@@ -8,40 +8,42 @@
 void decode(machine_state* ms) {
   decoded_inst result;
   uint32_t inst = ms -> instrFetched;
-  result.cond = extractBits(inst, 28, 31); //Common to all instructions
-  if (extractBits(inst, 0, 31) == 0) {
+  result.cond = inst >> 28; //Common to all instructions
+  if (!inst) {
     result.type = HALT;
-  } else if (extractBits(inst, 26, 26) == 1) {
+  } else if ((inst >> 26) & 0x1) {
     result.type = DATA_TRANS;
-    result.I = extractBits(inst, 25, 25);
-    result.P = extractBits(inst, 24, 24);
-    result.U = extractBits(inst, 23, 23);
-    result.L = extractBits(inst, 20, 20);
-    result.Rn = extractBits(inst, 16, 19);
+    result.I = (inst >> 25) & 0x1;
+    result.P = (inst >> 24) & 0x1;
+    result.U = (inst >> 23) & 0x1;
+    result.L = (inst >> 20) & 0x1;
+    result.Rn = (inst >> 16) & 0xF;
     if (result.Rn == 15) { //Checks if Rn is PC
       result.Rn -= 2; //According to implementation, PC is at index 13 of regs
     }
-    result.Rd = extractBits(inst, 12, 15);
-    result.operand_offset = extractBits(inst, 0, 11);
-  } else if (extractBits(inst, 27, 27) == 1) {
+    result.Rd = (inst >> 12 ) & 0xF;
+    result.operand_offset = inst & 0xFFF;
+  } else if ((inst >> 27) & 0x1) {
     result.type = BRANCH;
-    result.branch_offset = extractBits(inst, 0, 23);
-  } else if (extractBits(inst, 4, 7) == 9) { //9 in binary is 1001
+    result.branch_offset = inst & 0xFFFFFF;
+  } else if (((inst >> 4) & 0xF) == 0x9) { //0x9 in binary is 1001
     result.type = MULT;
-    result.A = extractBits(inst, 21, 21);
-    result.S = extractBits(inst, 20, 20);
-    result.Rd = extractBits(inst, 16, 19);
-    result.Rn = extractBits(inst, 12, 15);
-    result.Rs = extractBits(inst, 8, 11);
-    result.Rm = extractBits(inst, 0, 3);
+    result.A = (inst >> 21) & 0x1;
+    result.S = (inst >> 20) & 0x1;
+    result.Rd = (inst >> 16) & 0xF;
+    result.Rn = (inst >> 12) & 0xF;
+    result.Rs = (inst >> 8) & 0xF;
+    result.Rm = inst & 0xF;
   } else {
     result.type = DATA_PROC;
-    result.I = extractBits(inst, 25, 25);
-    result.operation_code = extractBits(inst, 21, 24);
-    result.S = extractBits(inst, 20, 20);
-    result.Rn = extractBits(inst, 16, 19);
-    result.Rd = extractBits(inst, 12, 15);
-    result.operand_offset = extractBits(inst, 0, 11);
+    result.I = (inst >> 25) & 0x1;
+    result.operation_code = (inst >> 21) & 0xF;
+    result.S = (inst >> 20) & 0x1;;
+    result.Rn = (inst >> 16) & 0xF;
+    result.Rd = (inst >> 12) & 0xF;
+    result.operand_offset = inst & 0xFFF;
   }
   ms->instrToExecute = result;
 }
+
+
--- src/emulate.c
+++ src/emulate.c
@@ -1,20 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 #include "utils.h"
 #include "types.h"
 #include "decode.h"
 
-void binLoad(FILE* fp, uint8_t* array) {
+void binLoad(FILE *fp, uint8_t *array) {
 
-  int read = 0; //Number of instructions read
+    int read = 0; //Number of instructions read
 
-  uint8_t* ptr = array; //Helper pointer to store instructions into array
+    uint8_t *ptr = array; //Helper pointer to store instructions into array
 
-  while (fread(ptr, 1, 1, fp) == 1) {
-    read++;
-    ptr++;
-  }
+    while (fread(ptr, 1, 1, fp) == 1) {
+        read++;
+        ptr++;
+    }
 
 /*
   //Debugging
@@ -41,51 +42,51 @@ void update_processor_state(machine_state* ms) {
 */
 
 int main(int argc, char **argv) {
-  /*
-  Machine state when turned on is zero
-  15 registers initialised to zero
-  Memory of ARM emulator has 64KB, initialised to zero
-  */
-  machine_state* ms = (machine_state*) calloc(sizeof(machine_state), 1);
-  // check for successful memory allocation
-  if (!ms) {
-    fprintf(stderr, "Unsuccessful Memory Allocation");
-    return EXIT_FAILURE;
-  }
-
-  //Opens file, argv[1] to skip space character
-  FILE* fp;
-  if (!(fp = fopen(argv[1], "rb"))) {
-    perror("Cannot Open File");
-    terminate(ms);
-  };
-
-  fseek(fp, 0, SEEK_END); //Navigates to end of file to get size
-  int no_of_inst = (ftell(fp) / 4) + 1; //+1 to add HALT inst at the end
-  rewind(fp); // Returns to beginning of file
-
-
-  binLoad(fp, ms->mem);
-  fclose(fp);
-
-  while (ms->regs.PC <= 4 * no_of_inst) {
-    switch (ms->ps) {
-      case (DECODED):
-        execute(ms);
-      case (FETCHED):
-        decode(ms);
-      case (EMPTY):
-        ms->instrFetched = load_word(ms->regs.PC, ms);
-        ms->regs.PC += 4;
-        // update_processor_state(ms);
-        ms->ps = !ms->ps ? FETCHED : DECODED;
-      // default case?
+    /*
+    Machine state when turned on is zero
+    15 registers initialised to zero
+    Memory of ARM emulator has 64KB, initialised to zero
+    */
+    machine_state *ms = (machine_state *) calloc(sizeof(machine_state), 1);
+    // check for successful memory allocation
+    if (!ms) {
+        fprintf(stderr, "Unsuccessful Memory Allocation");
+        return EXIT_FAILURE;
     }
-  }
 
-	execute(ms);
+    //Opens file, argv[1] to skip space character
+    FILE *fp;
+    if (!(fp = fopen(argv[1], "rb"))) {
+        perror("Cannot Open File");
+        terminate(ms);
+    };
 
-  free(ms);
+    fseek(fp, 0, SEEK_END); //Navigates to end of file to get size
+    int no_of_inst = (ftell(fp) / 4) + 1; //+1 to add HALT inst at the end
+    rewind(fp); // Returns to beginning of file
 
-  return EXIT_SUCCESS;
+
+    binLoad(fp, ms->mem);
+    fclose(fp);
+
+    while (ms->regs.PC <= 4 * no_of_inst) {
+        switch (ms->ps) {
+            case (DECODED):
+                execute(ms);
+            case (FETCHED):
+                decode(ms);
+            case (EMPTY):
+                ms->instrFetched = load_word(ms->regs.PC, ms);
+                ms->regs.PC += 4;
+                // update_processor_state(ms);
+                ms->ps = !ms->ps ? FETCHED : DECODED;
+                // default case?
+        }
+    }
+
+    execute(ms);
+
+    free(ms);
+
+    return EXIT_SUCCESS;
 }
