b03951493603f623534c8e07ce92009d8a7d8dcd
Ivy Tam <ivytam@Ivys-MacBook-Pro.local>
2020-06-13 03:00:00 +0800

    Branch but unsure how to carry current address

--- src/assembler/branch.c
+++ src/assembler/branch.c
@@ -1,7 +1,28 @@
 #include "assemble.h"
+
+uint32_t signed_to_twos_complement(int32_t value) {
+    uint32_t result = abs(value);
+    if (value < 0) {
+        result = ~result + 1;
+    }
+    return result;
+}
+
 uint32_t assemble_br(tokenized_instr_t * token_i, symbol_table_t * symbolTable){
     // bits 24-27 is always 1010
     uint32_t result = 10 << 24;
+    uint16_t label_address;
+    char *label = token_i->operands[0];
+    if(!map(symbolTable, label, label_address)){
+        perror("Label does not exist");
+        exit(EXIT_FAILURE);
+    }
+// instruction no in tokenized instr?
+    uint32_t offset = ((int32_t)label_address - ((int32_t)instruction_no * 4) - 8) >> 2;
+    // off by 8 bytes due to ARM pipeline
+    offset = signed_to_twos_complement(offset) & 0xFFFFFF;
+    // mask with 0xFFFFFF to get lower 24 bits
+    result |= offset;
     // set the condition codes (cc)
     switch(token_i->mnemonic){
         case BEQ:
