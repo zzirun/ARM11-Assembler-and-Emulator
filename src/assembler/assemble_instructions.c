f3c258970f39a5092d1dbe4857c192bbe95d15df
Tee, Wei <wei.tee19@imperial.ac.uk>
2020-06-14 01:58:04 +0800

    Modified condition for looping over program

--- src/assembler/assemble_instructions.c
+++ src/assembler/assemble_instructions.c
@@ -5,10 +5,10 @@ void assemble_instructions(program_t *prog, symbol_table_t *st) {
   prog->curr = prog->head->next;
 	//prog->curr != prog->last_instr->next
 	//Brandon : Changed the condition here just to get it running, not sure if correct according to what you guys had in mind
-  for (; prog->curr != prog->tail; prog->curr = prog->curr->next) {
+  // shld be last_instr->next since tail may belong to a data type added by ldr in SDT, stop before getting to data part of program
+  for (; prog->curr != prog->last_instr->next; prog->curr = prog->curr->next) {
 	  prog->curr->instr_str->assemble(prog, st);
   }
-	prog->curr->instr_str->assemble(prog, st);
 }
 
 // Add assemble function for each instruction type
