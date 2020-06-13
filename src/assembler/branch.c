b4e29545859f71b2c0161b3e0a2d699fee1fc2ad
Ivy Tam <ivytam@Ivys-MacBook-Pro.local>
2020-06-13 16:33:38 +0800

    Updated branch with new parameters

--- src/assembler/branch.c
+++ src/assembler/branch.c
@@ -8,47 +8,49 @@ uint32_t signed_to_twos_complement(int32_t value) {
     return result;
 }
 
-uint32_t assemble_br(tokenized_instr_t * token_i, symbol_table_t * symbolTable){
+void assemble_br(instr_list_t il, symbol_table_t * symbolTable){
+    tokenized_instr_t *token_i = il->curr->tokenized_instr;
     // bits 24-27 is always 1010
-    uint32_t result = 10 << 24;
+    uint32_t bin_instr = 10 << 24;
+
     uint16_t label_address;
     char *label = token_i->operands[0];
     if(!map(symbolTable, label, label_address)){
         perror("Label does not exist");
         exit(EXIT_FAILURE);
     }
-// instruction no in tokenized instr?
-    uint32_t offset = ((int32_t)label_address - ((int32_t)instruction_no * 4) - 8) >> 2;
+    uint32_t offset = ((int32_t)label_address - (int32_t)il->curr->address - 8) >> 2;
     // off by 8 bytes due to ARM pipeline
     offset = signed_to_twos_complement(offset) & 0xFFFFFF;
     // mask with 0xFFFFFF to get lower 24 bits
-    result |= offset;
+    bin_instr |= offset;
+
     // set the condition codes (cc)
     switch(token_i->mnemonic){
         case BEQ:
             break; // cc : 0000
         case BNE:
-            result |= 1 << 28; // cc : 0001
+            bin_instr |= 1 << 28; // cc : 0001
             break;
         case BGE:
-            result |= 10 << 28; // cc : 1010
+            bin_instr |= 10 << 28; // cc : 1010
             break;
         case BLT:
-            result |= 11 << 28; // cc : 1011
+            bin_instr |= 11 << 28; // cc : 1011
             break;
         case BGT:
-            result |= 12 << 28; // cc : 1100
+            bin_instr |= 12 << 28; // cc : 1100
             break;
         case BLE:
-            result |= 13 << 28; // cc : 1101
+            bin_instr |= 13 << 28; // cc : 1101
             break;
         case B:
-            result |= 14 << 28; // cc : 1110
+            bin_instr |= 14 << 28; // cc : 1110
             break;
         default:
             assert(false);
     }
-
+    il->curr->binary_instr = bin_instr;
 }
 
 
